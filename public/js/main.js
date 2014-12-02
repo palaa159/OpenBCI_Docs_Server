b5659ffd09ef7f225ef39a079e09922d5123945a
root <root@ip-172-31-30-38.us-west-2.compute.internal>
2014-12-02 18:08:37 +0000

    updat

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,23 @@
-
 var init = function() {
-	// highlight page
-	var currPath = window.location.pathname.substr(1, window.location.pathname.length);
-	var sectionToAdd = $('<div>').addClass('section');
-	mdList.forEach(function(item) {
-		if(item.filename === currPath) {
-			sectionToAdd.append($('<a class="selected mainMenuItem" href="/'+item.filename+'">').html(item.title + '<br>'));
-		} else {
-			sectionToAdd.append($('<a class="notSelected mainMenuItem" href="/'+item.filename+'">').html(item.title + '<br>'));
-		}
-	});
-	$('.menubar').prepend(sectionToAdd);
+    // highlight page
+    var currPath = window.location.pathname.substr(1, window.location.pathname.length);
+    var sectionToAdd = $('<div>').addClass('section');
+    mdList.forEach(function(item) {
+        if (item.filename === currPath) {
+            sectionToAdd.append($('<a class="selected mainMenuItem" href="/' + item.filename + '">').html(item.title + '<br>'));
+        } else {
+            sectionToAdd.append($('<a class="notSelected mainMenuItem" href="/' + item.filename + '">').html(item.title + '<br>'));
+        }
+    });
+    $('.menubar').prepend(sectionToAdd);
+    // reposition text
+    $(window).on('flatdoc:ready', function() {
+        // console.log('flatdoc ready');
+        var allImgs = $('.content-img');
+        $.each(allImgs, function(i, v) {
+            $(v).parent().addClass('img-js');
+        });
+    });
 };
 
 init();
